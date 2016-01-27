05e645a4dc171585f247344252934cc5f239a096
Thomas Stringer <sqlsalt@outlook.com>
2016-01-26 22:53:38 -0500

    Added dummy module

---
+++ simple-math.js
@@ -0,0 +1,9 @@
+var addOne = function (inputNumber) {
+  return inputNumber + 1;
+};
+var addFive = function (inputNumber) {
+  return inputNumber + 5;
+};
+
+module.exports.addOne = addOne;
+module.exports.addFive = addFive;
